a4bac9ea96498c976c6af56838ba09f700d7ea17
Sumanth Kumar <72268081+SumanthK1@users.noreply.github.com>
2022-05-18 16:54:13 -0400

    Add files via upload

---
+++ TicTacToe/Calcunow/Calcunow/Shared/CalcunowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcunowApp.swift
+//  Shared
+//
+//  Created by Naresh Kumar on 2021-07-21.
+//
+
+import SwiftUI
+
+@main
+struct CalcunowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TicTacToe/Calcunow/Calcunow/Tic/Shared/TicApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicApp.swift
+//  Shared
+//
+//  Created by Naresh Kumar on 2021-07-28.
+//
+
+import SwiftUI
+
+@main
+struct TicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
